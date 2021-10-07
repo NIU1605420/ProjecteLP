57f12d4697306ebfe3787da06ab48f23b8b1f3bc
arnauserra24 <72455057+arnauserra24@users.noreply.github.com>
2021-10-07 10:14:34 +0200

    Feature/tasca3 (#5)
    * Comment prova
    
    * Creació PuntDeInteresRestaurantSolucio i PuntDeInteresBotigaSolucio
    
    Co-authored-by: Carlos Alejandro Melis Curbelo <carlos.melis@airplane.solutions>
    Co-authored-by: Arnau Serra <arnauserra24@hotmail.com>

---
+++ LP-OSM-2122-DLL/PuntDeInteresBotigaSolucio.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "PuntDeInteresBase.h"
+
+using namespace std;
+
+class PuntDeInteresBotigaSolucio: public PuntDeInteresBase
+{
+public:
+	PuntDeInteresBotigaSolucio() {};
+	~PuntDeInteresBotigaSolucio() {};
+
+	string getName();
+	unsigned int getColor();
+private:
+
+};
---
+++ LP-OSM-2122-DLL/PuntDeInteresRestaurantSolucio.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "PuntDeInteresBase.h"
+using namespace std;
+
+class PuntDeInteresRestaurantSolucio: public PuntDeInteresBase
+{
+public:
+	PuntDeInteresRestaurantSolucio() {};
+	~PuntDeInteresRestaurantSolucio() {};
+
+	string getName();
+	unsigned int getColor();
+private:
+
+};
