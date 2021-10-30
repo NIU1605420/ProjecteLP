c34283710dec92b0e772038ef5e4594ad2ba068a
Carlos Alejandro Melis Curbelo <carlos.melis@airplane.solutions>
2021-10-30 16:54:33 +0200

    Primeros cambios MapaSolucio

---
+++ LP-OSM-2122-DLL/CamiSolucio.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Common.h"
+#include "CamiBase.h"
+
+using namespace std;
+
+class CamiSolucio : public CamiBase 
+{
+public:
+	CamiSolucio() {}
+	~CamiSolucio() {}
+
+	vector<Coordinate> getCamiCoords() {}
+
+private:
+
+};
